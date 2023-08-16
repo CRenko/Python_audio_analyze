cd0548685d9b581477076e769bd9053f70e3d74b
CRenko <1832929730@qq.com>
2023-08-16 22:51:23 +0800

    变更

--- audio_analyze/Test.py
+++ audio_analyze/Test.py
@@ -1,4 +1,3 @@
-
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 from Timefeature import *
@@ -6,7 +5,7 @@ import numpy as np
 
 fs, data = wavfile.read('恋音と雨空鼓点.wav')
 
-inc = 1000
+inc = 100
 # 窗口长度
 wlen = 1000
 # 取一个长度为wlen的汉明窗
@@ -20,30 +19,48 @@ EN = STEn(data[:, 0], wlen, inc)
 
 frameTime = FrameTimeC(len(EN), wlen, inc, fs)
 a = frameTime[EN > (np.max(EN) * 0.4)]
-data = open("data.txt", 'w+')
-
 # value设置阈值,name设置导出的数据的组名
 value = 0.3
 name = "\"song1\""
 # 最小有效短时能量
 minEN = np.max(EN) * value
+printers = []
 start = EN[0]
-
 count = 0
+doubles = 0
 n = 1
-print("{%s:[" % (name), file=data)
 for en in EN:
     if en >= minEN:
         count += 1
+        if en >= minEN * (5.0 / 3):
+            doubles += 1
     elif count < 5:
         start = en
         count = 0
-    else:
-        print("{\"time\":%.1ff, \"length\":%d, \"track\":%d}," % (frameTime[np.where(EN==start)], 1 + (count - 5) // 5, n),
-              file=data)
+    elif doubles <= count / 2:
+        printers.append([frameTime[np.where(EN == start)], 1 + (count - 5) // 5, n])
         n += 1
         if n > 6:
             n = 1
+        count = 0
+    else:
+        printers.append([frameTime[np.where(EN == start)], 1 + (count - 5) // 5, n])
+        printers.append([frameTime[np.where(EN == start)], 1 + (count - 5) // 5, 7 - n])
+        n += 1
+        if n > 6:
+            n = 1
+        count = 0
+data = open("data.txt", 'w+')
+print("{%s:[" % name, file=data)
+for printer in printers:
+    if printer != printers[-1]:
+        print("{\"time\":%.1ff, \"length\":%d, \"track\":%d}," % (
+            printer[0], printer[1], printer[2]),
+              file=data)
+    else:
+        print("{\"time\":%.1ff, \"length\":%d, \"track\":%d}" % (
+            printer[0], printer[1], printer[2]),
+              file=data)
 
 print("]}", file=data)
 data.close()
